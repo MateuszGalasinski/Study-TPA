5ac09f745a843a921b7b3c834cb01ce7320fe0c8
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-11-07 21:32:41 +0100

    Test for getpath command

--- SharedUILogic.Tests/Given_MainViewModel/GivenMainViewModel.cs
+++ SharedUILogic.Tests/Given_MainViewModel/GivenMainViewModel.cs
@@ -16,14 +16,27 @@ namespace SharedUILogic.Tests.Given_MainViewModel
         protected Mock<IStoreProvider> _storeProviderMock;
         protected Mock<IMapper<AssemblyMetadataStore, TreeItem>> _mapperMock;
 
+        protected const string FilePath = "somePath";
+
         [SetUp]
         public void Given()
         {
+            _filePathGetterMock = new Mock<IFilePathGetter>(MockBehavior.Strict);
+            _loggerMock = new Mock<ILogger>(MockBehavior.Strict);
+            _storeProviderMock = new Mock<IStoreProvider>(MockBehavior.Strict);
+            _mapperMock = new Mock<IMapper<AssemblyMetadataStore, TreeItem>>(MockBehavior.Strict);
+
             _context = new MainViewModel(
                 _filePathGetterMock.Object,
                 _loggerMock.Object,
                 _storeProviderMock.Object,
                 _mapperMock.Object);
+        }
+
+        public void With_FilePathGetter()
+        {
+            _filePathGetterMock.Setup(m => m.GetFilePath())
+                .Returns(FilePath);
         }
     }
 }
