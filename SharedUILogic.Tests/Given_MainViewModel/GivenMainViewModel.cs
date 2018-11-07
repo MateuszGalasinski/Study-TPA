e08a275dec5a11b03ad2ff21a4d3fd4a21adb6e3
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-11-07 21:06:04 +0100

    in work

---
+++ SharedUILogic.Tests/Given_MainViewModel/GivenMainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Core.Components;
+using Core.Model;
+using Moq;
+using NUnit.Framework;
+using SharedUILogic.Model;
+using SharedUILogic.ViewModel;
+
+namespace SharedUILogic.Tests.Given_MainViewModel
+{
+    [TestFixture]
+    public class GivenMainViewModel
+    {
+        protected MainViewModel _context;
+        protected Mock<IFilePathGetter> _filePathGetterMock;
+        protected Mock<ILogger> _loggerMock;
+        protected Mock<IStoreProvider> _storeProviderMock;
+        protected Mock<IMapper<AssemblyMetadataStore, TreeItem>> _mapperMock;
+
+        [SetUp]
+        public void Given()
+        {
+            _context = new MainViewModel(
+                _filePathGetterMock.Object,
+                _loggerMock.Object,
+                _storeProviderMock.Object,
+                _mapperMock.Object);
+        }
+    }
+}
