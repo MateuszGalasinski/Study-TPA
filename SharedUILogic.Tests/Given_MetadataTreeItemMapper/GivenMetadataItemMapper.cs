2970a9374424235b6aa58fc78512fad0f37378c6
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-11-07 18:59:29 +0100

    added tests project for sharedUILogic, with tests for mapper

---
+++ SharedUILogic.Tests/Given_MetadataTreeItemMapper/GivenMetadataItemMapper.cs
@@ -0,0 +1,136 @@
+﻿using Core.Constants;
+using Core.Model;
+using NUnit.Framework;
+using SharedUILogic.Services;
+using System;
+using System.Collections.Generic;
+
+namespace SharedUILogic.Tests.Given_MetadataTreeItemMapper
+{
+    [TestFixture]
+    public class GivenMetadataItemMapper
+    {
+        protected MetadataTreeItemMapper _context;
+        protected AssemblyMetadata _assemblyMetadata;
+        protected AssemblyMetadataStore _storage;
+
+        protected const string _assemblyName = "TestAssembly";
+        protected const string _namespaceName = "TestNamespace";
+        protected const string _typeName = "TestType";
+        protected const string _secondTypeName = "2TestType";
+        protected const string _thirdTypeName = "3TestType";
+        protected const string _propertyName = "TestProperty";
+        protected const string _methodName = "TestMethod";
+        protected const string _parameterName = "TestParameter";
+        protected const Accessibility _typeAccessLevel = Accessibility.Public;
+        protected const Accessibility _methodAccessLevel = Accessibility.Public;
+
+        [SetUp]
+        public void Given()
+        {
+            _context = new MetadataTreeItemMapper();
+        }
+
+        protected void With_AssemblyMetadata()
+        {
+            _assemblyMetadata = new AssemblyMetadata()
+            {
+                Id = _assemblyName,
+                Name = _assemblyName,
+                Namespaces = new List<NamespaceMetadata>()
+            };
+
+            _storage = new AssemblyMetadataStore(_assemblyMetadata);
+        }
+
+
+        protected void With_NamespaceMetaData()
+        {
+            NamespaceMetadata namespaceData = new NamespaceMetadata()
+            {
+                Id = _namespaceName,
+                Name = _namespaceName,
+                Types = new List<TypeMetadata>()
+            };
+
+            (_assemblyMetadata.Namespaces as List<NamespaceMetadata>).Add(namespaceData);
+            _storage.NamespacesDictionary.Add(namespaceData.Id, namespaceData);
+        }
+
+        protected void With_TypeMetaData()
+        {
+            NamespaceMetadata namespaceMetadata = _storage.NamespacesDictionary[_namespaceName];
+            TypeMetadata typeMetadata = CreateSimpleTypeMetadata(_typeName);
+            (namespaceMetadata.Types as List<TypeMetadata>).Add(typeMetadata);
+            _storage.TypesDictionary.Add(typeMetadata.Id, typeMetadata);
+        }
+
+        protected void With_PropertyMetadata()
+        {
+            TypeMetadata typeMetadata = _storage.TypesDictionary[_typeName];
+            typeMetadata.Kind = TypeKind.ClassType;
+
+            TypeMetadata propertyTypeMetadata = CreateSimpleTypeMetadata(_secondTypeName);
+            PropertyMetadata propertyMetadata = new PropertyMetadata()
+            {
+                Id = _propertyName,
+                Name = _propertyName,
+                TypeMetadata = propertyTypeMetadata
+            };
+            (typeMetadata.Properties as List<PropertyMetadata>).Add(propertyMetadata);
+            _storage.PropertiesDictionary.Add(propertyMetadata.Id, propertyMetadata);
+            _storage.TypesDictionary.Add(propertyTypeMetadata.Id, propertyTypeMetadata);
+        }
+
+        protected void With_VoidMethodMetadata(IEnumerable<ParameterMetadata> parameters)
+        {
+            TypeMetadata typeMetadata = _storage.TypesDictionary[_typeName];
+            typeMetadata.Kind = TypeKind.ClassType;
+
+            MethodMetadata methodMetadata = new MethodMetadata()
+            {
+                Id = _methodName,
+                Name = _methodName,
+                GenericArguments = new List<TypeMetadata>(),
+                Modifiers = new Tuple<Accessibility, IsAbstract, IsStatic, IsVirtual>(
+                    _methodAccessLevel,
+                    default(IsAbstract),
+                    default(IsStatic),
+                    default(IsVirtual)),
+                ReturnType = null,
+                Parameters = parameters
+            };
+
+            foreach (ParameterMetadata parameterMetadataDto in parameters)
+            {
+                _storage.ParametersDictionary.Add(parameterMetadataDto.Id, parameterMetadataDto);   
+                _storage.TypesDictionary.Add(parameterMetadataDto.TypeMetadata.Id, parameterMetadataDto.TypeMetadata);
+            }
+
+            (typeMetadata.Methods as List<MethodMetadata>).Add(methodMetadata);
+            _storage.MethodsDictionary.Add(methodMetadata.Id, methodMetadata);
+        }
+
+        protected TypeMetadata CreateSimpleTypeMetadata(string typeName)
+        {
+            return new TypeMetadata
+            {
+                Id = typeName,
+                Name = typeName,
+                NamespaceName = _namespaceName,
+                Modifiers = new Tuple<Accessibility, IsSealed, IsAbstract>(
+                    _typeAccessLevel,
+                    default(IsSealed),
+                    default(IsAbstract)),
+                Kind = default(TypeKind),
+                Attributes = new List<Attribute>(),
+                Properties = new List<PropertyMetadata>(),
+                Constructors = new List<MethodMetadata>(),
+                GenericArguments = new List<TypeMetadata>(),
+                ImplementedInterfaces = new List<TypeMetadata>(),
+                Methods = new List<MethodMetadata>(),
+                NestedTypes = new List<TypeMetadata>()
+            };
+        }
+    }
+}
---
+++ SharedUILogic.Tests/Given_MetadataTreeItemMapper/When_Map/WhenMap.cs
@@ -0,0 +1,193 @@
+﻿using Core.Model;
+using FluentAssertions;
+using NUnit.Framework;
+using SharedUILogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SharedUILogic.Tests.Given_MetadataTreeItemMapper.When_Map
+{
+    public class WhenMap : GivenMetadataItemMapper
+    {
+        private TreeItem _rootItem;
+
+        public void When_MapStorage()
+        {
+            try
+            {
+                Task.Run(() => { _rootItem = _context.Map(_storage); }).Wait();
+            }
+            catch (AggregateException)
+            {
+            }
+        }
+
+        [Test]
+        public void And_OnlyAssembly()
+        {
+            With_AssemblyMetadata();
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, false));
+        }
+
+        [Test]
+        public void And_AssemblyWithEmptyNamespace()
+        {
+            With_AssemblyMetadata();
+            With_NamespaceMetaData();
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, true)
+            {
+                Children = { new TreeItem($"Namespace: {_namespaceName}", false)}
+            });
+        }
+
+        [Test]
+        public void And_AssemblyWithNamespaceAndType()
+        {
+            With_AssemblyMetadata();
+            With_NamespaceMetaData();
+            With_TypeMetaData();
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, true)
+            {
+                Children =
+                {
+                    new TreeItem($"Namespace: {_namespaceName}", true)
+                    {
+                        Children = { new TreeItem($"Enum: {_typeName}", false)}
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void And_AssemblyWithNamespaceAndTypeWithProperty()
+        {
+            With_AssemblyMetadata();
+            With_NamespaceMetaData();
+            With_TypeMetaData();
+            With_PropertyMetadata();
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, true)
+            {
+                Children =
+                {
+                    new TreeItem($"Namespace: {_namespaceName}", true)
+                    {
+                        Children =
+                        {
+                            new TreeItem($"Class: {_typeName}", true)
+                            {
+                                Children =
+                                {
+                                    new TreeItem($"Property: {_propertyName}", true)
+                                    {
+                                        Children = { new TreeItem($"Enum: {_secondTypeName}", false)}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void And_AssemblyWithNamespace_AndTypeWithVoidMethod_WithNoParameters()
+        {
+            With_AssemblyMetadata();
+            With_NamespaceMetaData();
+            With_TypeMetaData();
+            With_VoidMethodMetadata(new List<ParameterMetadata>());
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, true)
+            {
+                Children =
+                {
+                    new TreeItem($"Namespace: {_namespaceName}", true)
+                    {
+                        Children =
+                        {
+                            new TreeItem($"Class: {_typeName}", true)
+                            {
+                                Children =
+                                {
+                                    new TreeItem($"Public void {_methodName}", false)
+                                    {
+                                       
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void And_AssemblyWithNamespace_AndTypeWithVoidMethod_WithParameters()
+        {
+            With_AssemblyMetadata();
+            With_NamespaceMetaData();
+            With_TypeMetaData();
+            With_VoidMethodMetadata(new List<ParameterMetadata>()
+            {
+                new ParameterMetadata(_parameterName, CreateSimpleTypeMetadata(_thirdTypeName))
+                {
+                    Id = _parameterName
+                }
+            });
+
+            When_MapStorage();
+
+            Then_TreeShouldBe(new TreeItem(_assemblyName, true)
+            {
+                Children =
+                {
+                    new TreeItem($"Namespace: {_namespaceName}", true)
+                    {
+                        Children =
+                        {
+                            new TreeItem($"Class: {_typeName}", true)
+                            {
+                                Children =
+                                {
+                                    new TreeItem($"Public void {_methodName}", true)
+                                    {
+                                        Children =
+                                        {
+                                            new TreeItem($"Parameter: {_parameterName}", true)
+                                            {
+                                                Children =
+                                                {
+                                                    new TreeItem($"Enum: {_thirdTypeName}", false)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        public void Then_TreeShouldBe(TreeItem correctRoot)
+        {
+            _rootItem.Should().BeEquivalentTo(correctRoot);
+        }
+    }
+}
