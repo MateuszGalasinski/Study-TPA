f8cbd9d7063ec29377706865de8a46e50772a928
Mateusz Gałasiński <mateusz.galasinski@protonmail.com>
2018-11-07 22:10:54 +0100

    Feature/commandtests (#10)
    * in work
    
    * Test for getpath command
    
    * test for load metadata command

---
+++ SharedUILogic.Tests/Given_MainViewModel/GivenMainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Core.Components;
+using Core.Model;
+using Moq;
+using NUnit.Framework;
+using SharedUILogic.Model;
+using SharedUILogic.ViewModel;
+using System.Collections.Generic;
+
+namespace SharedUILogic.Tests.Given_MainViewModel
+{
+    [TestFixture]
+    public class GivenMainViewModel
+    {
+        protected MainViewModel _context;
+        protected Mock<IFilePathGetter> _filePathGetterMock;
+        protected Mock<ILogger> _loggerMock;
+        protected Mock<IStoreProvider> _storeProviderMock;
+        protected Mock<IMapper<AssemblyMetadataStore, TreeItem>> _mapperMock;
+
+        protected const string FilePath = "somePath";
+        protected AssemblyMetadataStore Store { get; set; }
+        protected TreeItem TreeRoot { get; set; }
+
+        [SetUp]
+        public void Given()
+        {
+            _filePathGetterMock = new Mock<IFilePathGetter>(MockBehavior.Strict);
+            _loggerMock = new Mock<ILogger>(MockBehavior.Strict);
+            _storeProviderMock = new Mock<IStoreProvider>(MockBehavior.Strict);
+            _mapperMock = new Mock<IMapper<AssemblyMetadataStore, TreeItem>>(MockBehavior.Strict);
+
+            _context = new MainViewModel(
+                _filePathGetterMock.Object,
+                _loggerMock.Object,
+                _storeProviderMock.Object,
+                _mapperMock.Object);
+        }
+
+        public void With_FilePathGetter()
+        {
+            _filePathGetterMock.Setup(m => m.GetFilePath())
+                .Returns(FilePath);
+        }
+
+        public void With_AssemblyMetadataStore()
+        {
+            Store = new AssemblyMetadataStore(new AssemblyMetadata()
+            {
+                Id = "id",
+                Name = "name",
+                Namespaces = new List<NamespaceMetadata>()
+            });
+        }
+
+        public void With_TreeRoot()
+        {
+            TreeRoot = new TreeItem("Name", false);
+        }
+
+        public void With_Mapper()
+        {
+            _mapperMock.Setup(m => m.Map(It.IsAny<AssemblyMetadataStore>()))
+                .Returns(TreeRoot);
+        }
+
+        public void With_StoreProvider()
+        {
+            _storeProviderMock.Setup(m => m.GetAssemblyMetadataStore(It.IsAny<string>()))
+                .Returns(Store);
+        }
+    }
+}
